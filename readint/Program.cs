5da753416c932ceff55cc257593e5e04d0d7d85a
leatherbullet <al.obnizov@yandex.ru>
2024-03-23 09:31:41 +0300

    Добавьте файлы проекта.

---
+++ readint/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace readint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string userInput;
+            bool isSuccess = true;
+
+            while (isSuccess)
+            {
+                Console.WriteLine("введите число:");
+                userInput = Console.ReadLine();
+
+                bool canConvert = int.TryParse(userInput, out number);
+
+                if(canConvert)
+                {
+                    Console.WriteLine($"преобразование '{userInput}' в {number} успешно");
+                    isSuccess = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Попытка преобразования '{userInput}' не получилось");
+                }
+            }
+        }
+    }
+}
